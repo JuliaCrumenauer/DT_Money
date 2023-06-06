13b51358b8da28464e0b40819c58ce9e0674cc95
Julia Crumenauer <julia.crumenauer@gmail.com>
2023-06-05 23:24:28 -0300

    conexão com api

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Transactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   
@@ -10,7 +11,11 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <Transactions />
+      <TransactionsProvider>
+
+        <Transactions />
+
+      </TransactionsProvider>
   </ThemeProvider>
   )
 }
